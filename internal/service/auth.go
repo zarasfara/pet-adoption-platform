7986cb692f990eda66293dc6c93c883b7c7ebf59
Eugene Osipov <85435988+zarasfara@users.noreply.github.com>
2023-11-01 09:03:22 +0500

    feat: :lock: Добавить хеширование пароля (#11)

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/zarasfara/pet-adoption-platform/internal/models"
 	"github.com/zarasfara/pet-adoption-platform/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct {
@@ -13,6 +14,22 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 	return &AuthService{repo: repo}
 }
 
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
 func (s AuthService) CreateUser(user models.User) error {
+	hashedPassword, err := hashPassword(user.Password)
+	if err != nil {
+		return nil
+	}
+	user.Password = hashedPassword
+	
 	return s.repo.CreateUser(user)
 }
